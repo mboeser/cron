08801bbed88137fb905d6d17038bdb6c0dccd563
Mike Boeser <mboeser@yahoo.com>
2019-11-26 23:22:24 -0600

    update uri

--- py_api.py
+++ py_api.py
@@ -1,4 +1,3 @@
-pip install requests
 import requests
-r = requests.get('https://data.heroku.com/dataclips/qhsrzhbxankazauihhmaacloijyp.json?access-token=3420cf85-099d-4550-9c6a-3919ba79cc7a')
+r = requests.get('https://www.google.com')
 print(r.text)
